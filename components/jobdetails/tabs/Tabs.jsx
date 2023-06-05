837130663d665ecb9f54c3b4512cf2fd0fbbe672
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2023-06-05 20:05:39 +0800

    Initial

---
+++ components/jobdetails/tabs/Tabs.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
+import { SIZES } from '../../../constants'
+
+import styles from './tabs.style'
+
+const TabButton = ({ name, activeTab, onHandleSearchType }) => {
+  return (
+    <TouchableOpacity style={styles.btn(name, activeTab)} onPress={onHandleSearchType}>
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+        data={tabs}
+        renderItem={({ item }) => {
+          return <TabButton name={item} activeTab={activeTab} onHandleSearchType={() => setActiveTab(item)} />
+        }}
+      />
+    </View>
+  )
+}
+
+export default Tabs
---
+++ hooks/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { RAPID_API_KEY } from '@env'
+
+const rapidApiKey = RAPID_API_KEY
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query }
+  }
+
+  const fetchData = async () => {
+    setIsLoading(true)
+
+    try {
+      const response = await axios.request(options)
+      setData(response.data.data)
+    } catch (error) {
+      setError(error)
+      alert('Something went wrong')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const refetch = () => {
+    fetchData()
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return { data, isLoading, error, refetch }
+}
+
+export default useFetch
